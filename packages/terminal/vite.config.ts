4cbaba27a7b4f43cfd12d8371254642818a18bbe
Enzo Notario <enzo.notario@gmail.com>
2023-01-29 12:24:17 -0300

    chore(terminal) types

--- packages/terminal/vite.config.ts
+++ packages/terminal/vite.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import dts from 'vite-plugin-dts'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -29,12 +29,11 @@ export default defineConfig({
         },
       },
     }),
-    // dts({
-    //   staticImport: true,
-    //   skipDiagnostics: false,
-    //   logDiagnostics: true,
-    //   rollupTypes: true,
-    //   insertTypesEntry: true,
-    // }),
+    dts({
+      staticImport: true,
+      skipDiagnostics: false,
+      rollupTypes: true,
+      insertTypesEntry: true,
+    }),
   ],
 })
