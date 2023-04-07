5beed652e2c265c6cfd263d27ba23cc526a757b7
enzonotario <enzo.notario@gmail.com>
2023-04-07 19:33:53 -0300

    chore(playground) hide tests header on settings.hideOptions

--- packages/playground/src/components/Playground.vue
+++ packages/playground/src/components/Playground.vue
@@ -1,22 +1,25 @@
 <script setup lang="ts">
 import VueSplitView from 'vue-split-view'
 import 'vue-split-view/dist/style.css'
+import { computed } from 'vue'
 import { useSettings } from '@/composables/useSettings'
 import PlaygroundEditor from '@/components/PlaygroundEditor.vue'
 import PlaygroundOutput from '@/components/PlaygroundOutput.vue'
 import PlaygroundTestsEditor from '@/components/PlaygroundTestsEditor.vue'
 
 const settings = useSettings().settings
+
+const codeEditorHeight = computed(() => (settings.value.hideOptions && settings.value.hideTests ? '100%' : 'calc(100% - 40px)'))
 </script>
 
 <template>
   <div class="flex flex-row w-full h-full">
-    <VueSplitView :direction="settings.layout" :a-max="settings.hideEditor ? 0 : undefined" class="overflow-hidden">
+    <VueSplitView :direction="settings.layout" :a-max="settings.hideEditor ? 0 : undefined" :b-max="0" class="overflow-hidden">
       <template v-if="!settings.hideEditor" #A>
         <VueSplitView
           direction="vertical"
-          :a-min="settings.hideTests ? 'calc(100% - 40px)' : '50%'"
-          :a-max="settings.hideTests ? 'calc(100% - 40px)' : '50%'"
+          :a-min="settings.hideTests ? `${codeEditorHeight}` : '50%'"
+          :a-max="settings.hideTests ? `${codeEditorHeight}` : '50%'"
           class="overflow-hidden"
         >
           <template v-if="!settings.hideEditor" #A>
