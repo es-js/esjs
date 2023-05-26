059414b0b7087d40242ba2014ad59d6e3fafc4e5
enzonotario <enzo.notario@gmail.com>
2023-05-26 14:56:43 -0300

    refactor(playground) tests pane size

--- packages/playground/src/components/PlaygroundView.vue
+++ packages/playground/src/components/PlaygroundView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { Pane, Splitpanes } from 'splitpanes'
+import { computed } from 'vue'
 import { useSettings } from '@/composables/useSettings'
 import OutputIframe from '@/output/OutputIframe.vue'
 import { useEditor } from '@/composables/useEditor'
@@ -8,6 +9,20 @@ import PreviewBar from '@/components/navigation/PreviewBar.vue'
 const settings = useSettings().settings
 
 const editor = useEditor()
+
+const testsPaneSize = computed(() => {
+  if (settings.value.hideTests)
+    return settings.value.layout === 'vertical' ? 10 : 5
+
+  return 50
+})
+
+const testsPaneMinSize = computed(() => {
+  if (settings.value.hideTests)
+    return settings.value.layout === 'vertical' ? 10 : 5
+
+  return 20
+})
 </script>
 
 <template>
@@ -26,14 +41,14 @@ const editor = useEditor()
         </Pane>
         <Pane
           v-if="!settings.hideOptions || !settings.hideTests"
-          :size="settings.hideTests ? 5 : 50"
-          min-size="8"
-          :max-size="settings.hideTests ? 5 : 50"
+          :size="testsPaneSize"
+          :min-size="testsPaneMinSize"
+          :max-size="testsPaneSize"
           :class="{
             'pl-2 pb-2': settings.layout === 'horizontal',
             'px-2': settings.layout === 'vertical',
           }"
-          class="w-full h-full flex flex-col"
+          class="w-full h-full flex flex-col space-y-1"
         >
           <TestsBar v-if="!settings.hideOptions || !settings.hideTests" :class="{ 'flex-grow': settings.hideTests }" />
           <TestsEditor v-if="!settings.hideTests" class="relative w-full h-full overflow-hidden rounded border border-light-900 dark:border-dark-400" />
@@ -50,6 +65,7 @@ const editor = useEditor()
           :class="{
             'px-2 pb-2': settings.layout === 'vertical',
             'pb-2 pr-2': settings.layout === 'horizontal',
+            'pl-2': settings.hideEditor,
           }"
           class="relative w-full overflow-hidden"
         />
