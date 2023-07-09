03fea8cdff02b8b2614751b5c41f92db0a388c13
enzonotario <enzo.notario@gmail.com>
2023-07-09 20:35:45 -0300

    change(compiler) add prueba to default imports

--- packages/compiler/src/constants.ts
+++ packages/compiler/src/constants.ts
@@ -1,3 +1,3 @@
-export const DEFAULT_IMPORTS = 'import { Terminal } from \'@es-js/terminal\''
-
 export const DEFAULT_TESTS_IMPORTS = 'import { prueba, pruebas, pruebasAsincronas, afirmar, assert, afirmarIguales, afirmarSimilares, afirmarMatricesIguales, afirmarObjetosIguales, afirmarMatricesSimilares, afirmarObjetosSimilares, afirmarVerdadero, afirmarFalso, afirmarDistinto } from \'@es-js/prueba\''
+
+export const DEFAULT_IMPORTS = `import { Terminal } from '@es-js/terminal' \n ${DEFAULT_TESTS_IMPORTS}`
