5b0b6e39ec8391630a02c32b6196094aef2f9003
enzonotario <enzo.notario@gmail.com>
2023-07-29 21:06:06 -0300

    change(ejecutar) use @es-js/sandbox

--- packages/ejecutar/src/main.ts
+++ packages/ejecutar/src/main.ts
@@ -1,122 +1,14 @@
-import './style.css'
-import { evalCode, hidePreview, init, previewTab, setupTheme } from './ejecutar.js'
-import 'iconify-icon'
+import { createSandbox} from "@es-js/sandbox"
+import {useShare} from "./useShare.ts"
 
-window.process = { env: {} }
-window.__modules__ = {}
+const share = useShare()
 
-window.__export__ = (mod, key, get) => {
-  Object.defineProperty(mod, key, {
-    enumerable: true,
-    configurable: true,
-    get,
-  })
-}
+const options = share.getOptionsFromUrl()
 
-window.__dynamic_import__ = (key) => {
-  return Promise.resolve(window.__modules__[key])
-}
-
-function registerEsJSPruebaEvents() {
-  const events = [
-    'esjs-prueba-success',
-    'esjs-prueba-error',
-    'esjs-pruebas-finished',
-  ]
-
-  events.map((event) => {
-    window.addEventListener(event, (args: any) => {
-      parent.postMessage({ action: event, data: JSON.stringify(args.detail) }, '*')
-    })
-  })
-}
-
-async function handle_message(ev: any) {
-  const { action, cmd_id, args } = ev.data
-
-  const send_message = (payload: any) => parent.postMessage({ ...payload }, ev.origin)
-  const send_reply = (payload: any) => send_message({ ...payload, cmd_id })
-  const send_ok = () => send_reply({ action: 'cmd_ok' })
-  const send_error = (message: string, stack: string) => send_reply({ action: 'cmd_error', message, stack })
-
-  if (action === 'eval') {
-    try {
-      await evalCode(args)
-
-      send_ok()
-    }
-    catch (error: any) {
-      send_error(error.message, error.stack)
-    }
-  }
-  else if (action === 'HIDE_PREVIEW') {
-    hidePreview(args)
-  }
-  else if (action === 'previewException') {
-    window._previewException(...args)
-  }
-  else if (action === 'PREVIEW') {
-    // console.warn(['----- DEPRECATED -----', 'Please use PREVIEW_TAB instead'])
-  }
-  else if (action === 'PREVIEW_TAB') {
-    previewTab(args)
-  }
-  else if (action === 'DARK_MODE') {
-    setupTheme(args === true ? 'dark' : 'light')
-  }
-}
-
-window.addEventListener('message', handle_message, false)
-
-window.onerror = function (
-  msg: string,
-  url: string,
-  lineNo: number,
-  columnNo: number,
-  error: Error | undefined,
-) {
-  // ignore errors from import map polyfill - these are necessary for
-  // it to detect browser support
-  if (msg.includes('module specifier “vue”')) {
-    // firefox only error, ignore
-    return false
-  }
-  if (msg.includes('Module specifier, \'vue')) {
-    // Safari only
-    return false
-  }
-  try {
-    parent.postMessage({ action: 'error', value: error }, '*')
-  }
-  catch (e) {
-    parent.postMessage({ action: 'error', value: msg }, '*')
-  }
-}
-
-window.addEventListener('unhandledrejection', (event) => {
-  if (event.reason.message.includes('Cross-origin')) {
-    event.preventDefault()
-    return
-  }
-  try {
-    parent.postMessage({ action: 'unhandledrejection', value: event.reason }, '*')
-  }
-  catch (e) {
-    parent.postMessage({ action: 'unhandledrejection', value: event.reason.message }, '*')
-  }
-})
-
-window._handleInfiniteLoopException = function (error: any) {
-  console.warn('¡Advertencia!: Se ha detectado un bucle infinito')
-  console.error(error)
-}
-
-window._previewException = function (line: number, column: number, message: string) {
-  console.warn(`¡Advertencia!: Se ha detectado un error en la línea ${line}`)
-}
-
-registerEsJSPruebaEvents()
-
-window.addEventListener('load', async () => {
-  await init()
+createSandbox('esjs-sandbox', {
+  theme: options.theme,
+  hidePreview: options.hidePreview,
+  previewTab: options.previewTab,
+  code: share.getCodeFromUrl(),
+  testsCode: share.getTestsCodeFromUrl()
 })
