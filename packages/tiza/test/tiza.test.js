fbcdfe07b9a4d1592e6627dc30e349438058fc76
enzonotario <enzo.notario@gmail.com>
2023-08-10 20:33:17 -0300

    chore(tiza) fix tests && release 1.0.0

--- packages/tiza/test/tiza.test.js
+++ packages/tiza/test/tiza.test.js
@@ -1,10 +1,10 @@
 import { describe, expect, test } from 'vitest'
-import tiza from '../src'
+import { tiza } from '../src'
 
 describe('tiza', () => {
   test('tiza.subrayado.azul', () => {
     const result = tiza.subrayado.fondoAzul.blanco('test')
-    const expectedResult = '<span style="white-space: pre;background-color:#3B82F6;color:#FFFFFF;"><u>test</u></span>'
+    const expectedResult = '<span style="white-space: pre;background-color:#3b82f6;color:#fff;"><u>test</u></span>'
 
     expect(result).toBe(expectedResult)
   })
