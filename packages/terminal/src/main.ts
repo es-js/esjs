d147accbc18714401a67dd3f91773896174a7529
enzonotario <enzo.notario@gmail.com>
2023-12-06 20:17:08 -0300

    chore: tiza tests && terminal import

--- packages/terminal/src/main.ts
+++ packages/terminal/src/main.ts
@@ -1,6 +1,4 @@
 import './components/es-terminal'
 import { usarTerminal } from './composables/usarTerminal'
 
-export { usarTerminal } from './composables/usarTerminal'
-
 export const Terminal = usarTerminal()
--- packages/tiza/test/tiza.test.js
+++ packages/tiza/test/tiza.test.js
@@ -4,7 +4,7 @@ import { tiza } from '../src'
 describe('tiza', () => {
   test('tiza.subrayado.azul', () => {
     const result = tiza.subrayado.fondoAzul.blanco('test')
-    const expectedResult = '<span style="white-space: pre;background-color:#3b82f6;color:#fff;"><u>test</u></span>'
+    const expectedResult = '<span style="white-space: pre-wrap;background-color:#3b82f6;color:#fff;"><u>test</u></span>'
 
     expect(result).toBe(expectedResult)
   })
