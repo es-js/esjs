c44e21434b02cf552c8212ce2d4a52938a358018
enzonotario <enzo.notario@gmail.com>
2024-02-28 19:07:50 -0300

    chore(LearnBar) links

--- apps/playground/components/learn/LearnBar.vue
+++ apps/playground/components/learn/LearnBar.vue
@@ -42,6 +42,34 @@ const mdAndUp = computed(() => grid.md || grid.lg || grid.xl)
       </div>
 
       <div class="flex flex-row justify-end items-center space-x-2">
+        <div class="hidden md:flex flex-row items-center space-x-4">
+          <NuxtLink
+            to="https://es.js.org"
+            target="_blank"
+            class="text-sm font-medium text-indigo-800 dark:text-indigo-200 flex flex-row items-center space-x-1"
+          >
+            <span>Documentación</span>
+            <UIcon
+              name="i-mdi-open-in-new"
+              class="w-4 h-4"
+            />
+          </NuxtLink>
+
+          <NuxtLink
+            to="https://editor.esjs.dev"
+            target="_blank"
+            class="text-sm font-medium text-indigo-800 dark:text-indigo-200 flex flex-row items-center space-x-1"
+          >
+            <span>Editor</span>
+            <UIcon
+              name="i-mdi-open-in-new"
+              class="w-4 h-4"
+            />
+          </NuxtLink>
+
+          <div class="h-4 w-0.5 bg-indigo-800 dark:bg-indigo-200" />
+        </div>
+
         <AppButton
           :icon="isDark ? 'i-mdi-weather-night' : 'i-mdi-weather-sunny'"
           icon-only
