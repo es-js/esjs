75a082318ec63d7eb06f09b46dd653599c7b354d
enzonotario <enzo.notario@gmail.com>
2024-04-16 22:30:22 -0300

    refactor(core) utils

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,5 +1,52 @@
-export { splitCodeImports } from './utils/split-code-imports'
-export { splitScriptTemplate } from './utils/split-script-template'
+const IMPORTS_REGEX = /import(?:["'\s]*([\w*{}\n\r\t, ]+)from\s*)?["'\s].*([@\w/_-]+)["'\s].*/g
+
+export interface SplitCodeImports {
+  codeWithoutImports: string
+  imports: string
+  hasImports: boolean
+}
+
+export function splitCodeImports(code: string): SplitCodeImports {
+  const codeWithoutImports: string = replaceImportsWith(code, '')
+  const imports: string = extractImportsFrom(code) ?? ''
+  const hasImports: boolean = Boolean(imports)
+
+  return {
+    codeWithoutImports,
+    imports,
+    hasImports,
+  }
+}
+
+function replaceImportsWith(code: string, replacement: string): string {
+  return code.replace(IMPORTS_REGEX, replacement).trim()
+}
+
+function extractImportsFrom(code: string): string | null {
+  return code.match(IMPORTS_REGEX)?.join('\n') ?? null
+}
+
+const FRONT_MATTER_DIVISOR = '---'
+
+export interface SplitScriptTemplate {
+  script: string | null
+  template: string | null
+}
+
+export function splitScriptTemplate(code: string): SplitScriptTemplate {
+  const trimmedCode: string = code.trim()
+  let script: string | null = trimmedCode
+  let template: string | null = null
+
+  if (trimmedCode.startsWith(FRONT_MATTER_DIVISOR)) {
+    script = trimmedCode.substring(trimmedCode.indexOf(FRONT_MATTER_DIVISOR) + 3)
+    script = script.substring(0, script.lastIndexOf(FRONT_MATTER_DIVISOR)).trim()
+
+    template = trimmedCode.substring(trimmedCode.lastIndexOf(FRONT_MATTER_DIVISOR) + 3).trim()
+  }
+
+  return { script, template }
+}
 
 export function invertMap(map: Map<string, string>): Map<string, string> {
   const invertedMap = new Map<string, string>()
---
+++ packages/core/test/utils.test.ts
@@ -0,0 +1,94 @@
+import { describe, expect, it } from 'vitest'
+import { invertMap, splitCodeImports, splitScriptTemplate } from '../src/utils'
+
+describe('utils', () => {
+  it('split code imports', () => {
+    const code = `
+import { foo } from 'bar'
+import { bar } from 'foo'
+import { foo as bar } from 'bar'
+import 'foo'
+import 'bar'
+
+const x = 1
+var y = 2
+let z = 3
+`
+
+    const { codeWithoutImports, imports, hasImports } = splitCodeImports(code)
+
+    expect(codeWithoutImports).toBe(`const x = 1
+var y = 2
+let z = 3`)
+
+    expect(imports).toBe(`import { foo } from 'bar'
+import { bar } from 'foo'
+import { foo as bar } from 'bar'
+import 'foo'
+import 'bar'`)
+
+    expect(hasImports).toBe(true)
+  })
+
+  it('split code without imports', () => {
+    const code = `
+const x = 1
+var y = 2
+let z = 3
+`
+
+    const { codeWithoutImports, imports, hasImports } = splitCodeImports(code)
+
+    expect(codeWithoutImports).toBe(`const x = 1
+var y = 2
+let z = 3`)
+
+    expect(imports).toBe('')
+
+    expect(hasImports).toBe(false)
+  })
+
+  it('split script template', () => {
+    const code = `
+---
+console.log('script')
+---
+
+<div>template</div>
+`
+
+    const { script, template } = splitScriptTemplate(code)
+
+    expect(script).toBe(`console.log('script')`)
+
+    expect(template).toBe(`<div>template</div>`)
+  })
+
+  it('split template without script', () => {
+    const code = `
+console.log('script')
+`
+
+    const { script, template } = splitScriptTemplate(code)
+
+    expect(script).toBe(`console.log('script')`)
+
+    expect(template).toBe(null)
+  })
+
+  it('invert map', () => {
+    const map = new Map<string, string>([
+      ['foo', 'bar'],
+      ['bar', 'baz'],
+      ['baz', 'foo'],
+    ])
+
+    const invertedMap = invertMap(map)
+
+    expect(invertedMap).toEqual(new Map<string, string>([
+      ['bar', 'foo'],
+      ['baz', 'bar'],
+      ['foo', 'baz'],
+    ]))
+  })
+})
