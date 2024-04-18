66ec5ec9d053b475ecc9975a797f4105cae21562
enzonotario <enzo.notario@gmail.com>
2024-04-17 22:29:54 -0300

    chore(core) keywords

--- packages/core/build.config.ts
+++ packages/core/build.config.ts
@@ -3,7 +3,7 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: [
     './src/index',
-    // './src/compiler/keywords',
+    './src/keywords',
     './src/utils',
   ],
   clean: true,
---
+++ packages/core/src/keywords.ts
@@ -0,0 +1,3 @@
+import { keywords as keywordsEsbabel } from '@es-js/esbabel/keywords'
+
+export const keywords = keywordsEsbabel
