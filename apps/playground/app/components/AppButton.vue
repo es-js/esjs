946f305ed34f48b7712728617c16fbc7d8bdbfdf
enzonotario <enzo.notario@gmail.com>
2024-08-16 22:40:16 -0300

    chore(AppButton) disabled dark color

--- apps/playground/app/components/AppButton.vue
+++ apps/playground/app/components/AppButton.vue
@@ -78,7 +78,7 @@ const color = computed(() => {
           class="space-x-2"
           :class="{
             'bg-teal-100 hover:bg-teal-200 disabled:bg-teal-50 dark:bg-teal-950 dark:hover:bg-teal-900 dark:disabled:bg-teal-950 text-teal-700 dark:text-teal-300 focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-teal-500 dark:focus-visible:ring-teal-400 border-teal-200 dark:border-teal-800': color === 'teal' && props.variant === 'outline',
-            'bg-black hover:bg-gray-900 disabled:bg-gray-800 dark:bg-white dark:hover:bg-gray-100 dark:disabled:bg-gray-900 text-white hover:text-white dark:text-gray-900 focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-black dark:focus-visible:ring-gray-900 border-black dark:border-gray-900': color === 'black' && props.variant === 'outline',
+            'bg-black hover:bg-gray-900 disabled:bg-gray-800 dark:bg-white dark:hover:bg-gray-100 dark:disabled:bg-gray-900 text-white hover:text-white dark:text-gray-900 disabled:dark:text-white focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-black dark:focus-visible:ring-gray-900 border-black dark:border-gray-900': color === 'black' && props.variant === 'outline',
             'bg-blue-100 hover:bg-blue-200 disabled:bg-blue-50 dark:bg-blue-950 dark:hover:bg-blue-900 dark:disabled:bg-blue-950 text-blue-700 dark:text-blue-300 hover:text-blue-800 dark:hover:text-blue-100 focus-visible:ring-2 focus-visible:ring-inset focus-visible:ring-blue-500 dark:focus-visible:ring-blue-400 border-blue-200 dark:border-blue-800': color === 'blue' && props.variant === 'outline',
             'dark:text-gray-100': color === 'gray' && props.variant === 'outline',
           }"
