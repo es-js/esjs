02f03351b92bebbff924cbd85d66d1d8522acc6a
enzonotario <enzo.notario@gmail.com>
2024-08-25 11:34:46 -0300

    fix(aprender) tipoDe examples

---
+++ packages/core/test/fixtures/keywords/tipoDe.js
@@ -0,0 +1,13 @@
+console.log(typeof 2);
+
+console.log(typeof "Martin Fierro");
+
+console.log(typeof true);
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+console.log(typeof {});
+
+console.log(typeof []);
