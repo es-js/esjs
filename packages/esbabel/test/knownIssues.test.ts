5a485909438356c91503db97524daa7c2f78e6dd
enzonotario <enzo.notario@gmail.com>
2024-09-19 20:12:10 -0300

    chore(prueba && terminal): update deps

--- packages/esbabel/test/knownIssues.test.ts
+++ packages/esbabel/test/knownIssues.test.ts
@@ -21,7 +21,7 @@ describe('known issues', () => {
   it.skip('fixed: does not compile variables named `get`', () => {
     // Deprecado: Ahora `mut` equivale a `let`, y `var` equivale a `var`.
     const esjs = `
-    var get = 'prueba'
+    mut get = 'prueba'
 `
 
     const js = `
