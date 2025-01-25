1ab1cc4d978f342af380dc7f7d77e4405db22d07
Trollhunters501PC <cpcreador@gmail.com>
2025-01-24 20:29:19 -0600

    Arreglar Errores

--- packages/core/test/fixtures/extras/simbolo.js
+++ packages/core/test/fixtures/extras/simbolo.js
@@ -1,12 +1,12 @@
 const sim1 = Symbol("foo")
 let sim2 = Symbol("foo")
-sim2.description
+sim2.description;
 Symbol()
 Symbol.for("foo")
 Symbol.keyFor(sim1)
-Symbol.hasInstance
-Symbol.iterator
-Symbol.toPrimitive
-Symbol.toStringTag
-Symbol.unscopables
+Symbol.hasInstance;
+Symbol.iterator;
+Symbol.toPrimitive;
+Symbol.toStringTag;
+Symbol.unscopables;
 Symbol.isConcatSpreadable;
