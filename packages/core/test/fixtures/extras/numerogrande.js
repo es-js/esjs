70c6168881bfaf1d7a5e377d71b5379abb36e99e
Trollhunters501PC <cpcreador@gmail.com>
2025-01-24 19:25:30 -0600

    Añadir NumeroGrande Tests y Arreglar Puente

---
+++ packages/core/test/fixtures/extras/numerogrande.js
@@ -0,0 +1,7 @@
+let myNum = new BigInt(1000);
+myNum.toStringTag;
+myNum.toLocaleString();
+myNum.toString();
+myNum.valueOf();
+BigInt.asIntN(64, myNum);
+BigInt.asUintN(64, myNum);
--- packages/core/test/fixtures/extras/puente.js
+++ packages/core/test/fixtures/extras/puente.js
@@ -1,2 +1,1 @@
 let myPuent = new Proxy();
-Proxy.revocable();
