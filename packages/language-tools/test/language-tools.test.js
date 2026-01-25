3bc98510a8f0f513789a6ec45b04c77e1db8b18c
enzonotario <enzo.notario@gmail.com>
2026-01-24 21:40:37 -0300

    chore: format

--- packages/language-tools/test/language-tools.test.js
+++ packages/language-tools/test/language-tools.test.js
@@ -1,9 +1,9 @@
-import { describe, it, expect } from 'vitest';
-import fs from 'node:fs';
-import '../src/index.js';
+import { describe, it, expect } from 'vitest'
+import fs from 'node:fs'
+import '../src/index.js'
 
 function readFile(path) {
-  return fs.readFileSync(path, 'utf-8');
+  return fs.readFileSync(path, 'utf-8')
 }
 
 describe('@es-js/language-tools', () => {
@@ -13,18 +13,18 @@ describe('@es-js/language-tools', () => {
     esjsConfig: './dist/esjs-language-configuration.json',
     esjsSyntax: './dist/esjs.tmLanguage.json',
     esvueConfig: './dist/esvue-language-configuration.json',
-    esvueSyntax: './dist/esvue.tmLanguage.json'
-  };
+    esvueSyntax: './dist/esvue.tmLanguage.json',
+  }
   const testFile = (description, filePath) => {
     it(`generates the correct ${description}`, () => {
-      const content = readFile(filePath);
-      expect(content).toMatchSnapshot();
-    });
-  };
-  testFile('reserved words table', TEST_FILES.reservedWords);
-  testFile('esjs code snippets', TEST_FILES.esjsSnippets);
-  testFile('esjs language configuration', TEST_FILES.esjsConfig);
-  testFile('esjs syntax', TEST_FILES.esjsSyntax);
-  testFile('esvue language configuration', TEST_FILES.esvueConfig);
-  testFile('esvue syntax', TEST_FILES.esvueSyntax);
-});
+      const content = readFile(filePath)
+      expect(content).toMatchSnapshot()
+    })
+  }
+  testFile('reserved words table', TEST_FILES.reservedWords)
+  testFile('esjs code snippets', TEST_FILES.esjsSnippets)
+  testFile('esjs language configuration', TEST_FILES.esjsConfig)
+  testFile('esjs syntax', TEST_FILES.esjsSyntax)
+  testFile('esvue language configuration', TEST_FILES.esvueConfig)
+  testFile('esvue syntax', TEST_FILES.esvueSyntax)
+})
