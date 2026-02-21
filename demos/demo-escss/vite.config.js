53608770249c4f8092857c5eeb7826537b24092b
enzonotario <enzo.notario@gmail.com>
2026-02-21 18:34:16 -0300

    chore(demo): add EsHTML and EsJS

--- demos/demo-escss/vite.config.js
+++ demos/demo-escss/vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import EsCSS from '@es-js/vite-plugin-escss'
+import EsHTML from '@es-js/vite-plugin-eshtml'
+import EsJS from '@es-js/vite-plugin-esjs'
 
 export default defineConfig({
-  plugins: [EsCSS()],
+  plugins: [
+    EsJS(),
+    EsCSS(),
+    EsHTML({
+      paginas: {
+        indice: {
+          plantilla: 'indice.eshtml',
+          entrada: 'fuente/indice.esjs',
+        },
+      },
+    }),
+  ],
 })
